150a2328f089d15b9c21acb5dc9baa3ba4e0e788
Kartik Shelke <kartikshelke2201@gmail.com>
2025-12-04 13:47:17 +0530

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,27 @@
+# predict.py
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import img_to_array
+import cv2
+
+# Load model
+model = tf.keras.models.load_model("model/agrobot.h5")
+
+# Class labels (update according to your model)
+CLASS_LABELS = ["fallen_leaf", "healthy", "powdery", "rust"]
+
+
+def preprocess_image(image):
+    img = cv2.resize(image, (224, 224))
+    img = img.astype("float32") / 255.0
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    return img
+
+
+def predict_disease(image):
+    processed = preprocess_image(image)
+    prediction = model.predict(processed)[0]
+    class_index = np.argmax(prediction)
+    confidence = prediction[class_index]
+    return CLASS_LABELS[class_index], float(confidence)
